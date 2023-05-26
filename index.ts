da5a2672987917b9174156720f3d639f2bd85ec4
validateITNikita <idshnicknickita@gmail.com>
2023-05-26 14:14:16 +0300

    feat: added Log Functions with types dependencies: added chalk-animation for animation to logs (not used) added vitest for tests

--- index.ts
+++ index.ts
@@ -1,6 +1,55 @@
-import chalk from 'chalk';
+import chalk, {
+    BackgroundColorName,
+    ColorName,
+    ModifierName,
+    modifierNames,
+} from "chalk";
+// import { Animation, rainbow } from "chalk-animation";
+
 const log = console.log;
 
-const main = ()  => {
-
+export enum LogLevel {
+    // debug,
+    info,
+    log,
+    warn,
+    error,
 }
+
+export const logText = (
+    text: string,
+    textColor: ColorName,
+    bgColor: BackgroundColorName,
+    modifier?: ModifierName
+) => {
+    log(chalk[textColor][bgColor](text));
+};
+
+export const Log = (text: string, level: LogLevel) => {
+    switch (level) {
+        // case LogLevel.debug:
+        //     logText(logText, );
+        //     break;
+        case LogLevel.info:
+            logText(text, 'white', 'bgCyanBright');
+            break;
+        case LogLevel.log:
+            logText(text, 'white', 'bgBlack');
+            break;
+        case LogLevel.warn:
+            logText(text, 'redBright', 'bgBlack');
+            break;
+        case LogLevel.error:
+            logText(text, 'red', 'bgBlack');
+            break;
+    }
+};
+
+const main = () => {
+    Log('LOG', LogLevel.log);
+    Log('INFO', LogLevel.info);
+    Log('WARN', LogLevel.warn);
+    Log('ERROR', LogLevel.error);
+};
+
+main();
